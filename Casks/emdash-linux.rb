aa2106b1568e0a358fc0538a3d72d14e61240cb0
Ahmed Adan <ahmed.adan@gmail.com>
2026-02-25 10:06:20 -0500

    feat(cask): add emdash-linux

---
+++ Casks/emdash-linux.rb
@@ -0,0 +1,49 @@
+cask "emdash-linux" do
+  arch intel: "x86_64"
+
+  version "0.4.18"
+  sha256 x86_64_linux: "7ad24b9dd5f6c081803660e3aae3828b07e1c2c1928808018e8a5214af42e782"
+
+  url "https://github.com/generalaction/emdash/releases/download/v#{version}/emdash-#{arch}.AppImage"
+  name "Emdash"
+  desc "Agentic development environment for running multiple coding agents in parallel"
+  homepage "https://emdash.sh/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on formula: "squashfs"
+
+  binary "squashfs-root/emdash", target: "emdash"
+  artifact "squashfs-root/usr/share/icons/hicolor/512x512/apps/emdash.png",
+           target: "#{Dir.home}/.local/share/icons/hicolor/512x512/apps/emdash.png"
+  artifact "squashfs-root/emdash.desktop",
+           target: "#{Dir.home}/.local/share/applications/emdash.desktop"
+
+  preflight do
+    appimage_path = "#{staged_path}/emdash-#{arch}.AppImage"
+    system "chmod", "+x", appimage_path
+    system appimage_path, "--appimage-extract", chdir: staged_path
+    FileUtils.rm(appimage_path)
+  end
+
+  postflight do
+    desktop_target = "#{Dir.home}/.local/share/applications/emdash.desktop"
+    if File.exist?(desktop_target)
+      desktop_content = File.read(desktop_target)
+      desktop_content.gsub!(/^Exec=AppRun/, "Exec=#{HOMEBREW_PREFIX}/bin/emdash")
+      desktop_content.gsub!(/^Icon=.*/,
+                            "Icon=#{Dir.home}/.local/share/icons/hicolor/512x512/apps/emdash.png")
+      File.write(desktop_target, desktop_content)
+    end
+  end
+
+  uninstall_postflight do
+    FileUtils.rm("#{Dir.home}/.local/share/icons/hicolor/512x512/apps/emdash.png")
+    FileUtils.rm("#{Dir.home}/.local/share/applications/emdash.desktop")
+  end
+
+  zap trash: "~/.config/emdash"
+end
